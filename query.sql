8c8790ba3f001dfee3d65a7985dd16d3861295c1
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-02-25 15:01:27 +0700

    feat: update quary 1 take data using id not name

--- query.sql
+++ query.sql
@@ -1,19 +1,20 @@
--- SELECT products.product_name, string_agg(variant_name, ',') as variant, size_name
--- FROM products
--- JOIN product_variant ON variant_name = product_variant.variant_name
--- JOIN product_size ON size_name = product_size.size_name
--- GROUP BY product_name, product_variant.variant_name, product_size.size_name
--- LIMIT 1;
-
-
 SELECT 
+    products.id,
     products.product_name,
     product_variant.variant_name,
     product_size.size_name,
-    (products.price + product_variant.add_price + product_size.size_price) AS final_price
+    (products.price + product_variant.add_price + product_size.size_price) * 2 AS total_price
 FROM products
-JOIN product_variant ON products.id = product_variant.product_id
-JOIN product_size ON products.id = product_size.product_id
+JOIN product_variant
+    ON products.id = product_variant.product_id
+JOIN product_size
+    ON products.id = product_size.product_id
 WHERE products.id = 1
-AND product_variant.variant_name = 'Ice'
-AND product_size.size_name = 'Large';
+AND product_variant.product_variant_id = 2
+AND product_size.product_size_id = 3;
+
+
+
+
+SELECT products.price, products.quantity, (products.price * products.quantity) AS sub_total
+FROM products
