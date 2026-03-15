bf706c01d1ae61e61d578c985d4015274500d19e
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-03-15 23:42:19 +0700

    feat: create dml ffor database

--- query.sql
+++ query.sql
@@ -1,60 +1,178 @@
-SELECT 
-    products.id,
-    products.product_name,
-    product_variant.variant_name,
-    product_size.size_name,
-    (products.price + product_variant.add_price + product_size.size_price) * 2 AS total_price
-FROM products
-JOIN product_variant
-    ON products.id = product_variant.product_id
-JOIN product_size
-    ON products.id = product_size.product_id
-WHERE products.id = 1
-AND product_variant.product_variant_id = 2
-AND product_size.product_size_id = 3;
+INSERT INTO category (category_id, category_name)
+VALUES
+(DEFAULT, 'kopi'), -- 1
+(DEFAULT, 'non kopi'), -- 2
+(DEFAULT, 'makanan'), -- 3
+(DEFAULT, 'minuman'), -- 4
+(DEFAULT, 'add on'), -- 5
+(DEFAULT, 'makanan asin'), -- 6
+(DEFAULT, 'makanan manis'), -- 7
+(DEFAULT, 'makanan ringan'), -- 8
+(DEFAULT, 'makanan berat'), -- 9
+(DEFAULT, 'roti'); -- 10
 
--- KEBUTUHAN DATA DI LANDING PAGES
--- 1. DATA CARD
--- analisa di table landing page kubutuhan querynya apa saja
--- table card di landing page:
--- data product
--- * image ~
--- * title ~
--- * description ~
--- * rating 
--- * price ~
+INSERT INTO discount (discount_rate, description, is_flash_sale)
+  VALUES
+    (10, 'Mother Day`s', TRUE), -- 1
+    (6, 'Ramadhan', TRUE), -- 2
+    (5, 'Idul Fitr', TRUE), -- 3
+    (9, 'Most Loyal Customoer', TRUE), -- 4
+    (0, 'No Discount', FALSE), -- 5
+    (1, 'Promo Februari', TRUE), -- 6
+    (0, 'Imlek', TRUE), -- 7
+    (10, 'Promo Februari', TRUE), -- 8
+    (9, 'Gift', FALSE), -- 9
+    (100, 'testing', FALSE); -- 10
 
--- 2. DATA TESTIMONEY
--- table testimoni di landing pages:
--- * images
--- * message
--- * reviews
+-- deprecated
+-- INSERT INTO products (id, product_name, product_desc, price, quantity, product_category, discount)
+--   VALUES
+--   (DEFAULT, 'kapal api late', 'Kopi Kapal Api Biasa', 15000, 30, 1, 2),
+--   (DEFAULT, 'Classic Coffe', 'Esspreso 2 shot with milk', 25000, 30, 1, 10),
+--   (DEFAULT, 'Lava cake', 'hidangan penutup (dessert)', 10000, 70, 2, 2),
+--   (DEFAULT, 'Brownis', 'makanan yang dipanggang yang berbentuk persegi, datar atau bar', 35000, 25, 2, 2),
+--   (DEFAULT, 'Muffin', 'Kue individu (sekali makan) yang dipanggang dalam cetakan berbentuk cangkir atau muffin tin.', 30000, 47, 2, 3),
+--   (DEFAULT, 'Kopi tubruk', 'Kopi Langsung Diseduh', 30000, 90, 2, 1),
+--   (DEFAULT, 'Melya', 'kopi berbahan dasar espresso yang dicampur dengan kakao bubuk dan madu.', 30000, 100, 2, 9),
+--   (DEFAULT, 'Café au lait', 'adalah campuran kopi hitam yang tajam dengan susu panas.', 30000, 10, 2, 5),
+--   (DEFAULT, 'Machiato', 'minuman kopi yang dibuat dengan mencampurkan espresso dengan susu.', 30000, 5, 2, 7),
+--   (DEFAULT, 'Esspreso', 'Kopi bertekanan Tinggi', 9000, 7, 2, 7),
+--   (DEFAULT, 'V60', 'No Description Yet', 15000, 9, 2, 8),
+--   (DEFAULT, 'Plain Milk', 'susu segar atau UHT tanpa tambahan perasa, gula, atau aroma, yang kaya akan protein, lemak utuh, vitamin, dan kalsium', 17500, 27, 4, 5),
+--   (DEFAULT, 'Flavour Milk', 'No Description Yet', 17000, 12, 4, 5),
+--   (DEFAULT, 'Soekapi Platter', 'No Description Yet', 70000, 9, 3, 5),
+--   (DEFAULT, 'Bitterballen', 'kudapan goreng khas Belanda yang sangat populer, berbentuk bola-bola kecil (sekitar 3-4 cm) dengan tekstur renyah di luar dan isian ragout daging yang lembut dan gurih di dalam', 35000, 27, 3, 5),
+--   (DEFAULT, 'Rissoles', 'pastri berisi daging cincang, sayuran, atau ragout yang dibungkus dadar tipis, dilapisi tepung panir, dan digoreng renyah', 28000, 29, 3,5),
+--   (DEFAULT, 'French Fries', 'hidangan yang dibuat dari potongan-potongan kentang yang digoreng dalam minyak goreng panas. ', 26000, 17, 3,5),
+--   (DEFAULT, 'Singkong Goreng', 'hidangan yang dibuat dari potongan-potongan kentang yang digoreng dalam minyak goreng panas. ', 26000, 15, 3,5),
+--   (DEFAULT, 'Pisang Goreng Salted Caramel', 'No Description Yet', 26000, 21, 3,5),
+--   (DEFAULT, 'Hot Chocolate', 'minuman hangat yang dibuat dengan melelehkan cokelat padat (seperti cokelat batangan, keping cokelat/chips, atau serutan) ke dalam susu atau air panas. ', 36000, 11, 2,5),
+--   (DEFAULT, 'Ice Chocolate', 'minuman cokelat dingin yang menyegarkan, terbuat dari campuran cokelat (bubuk atau sirup), susu, dan es batu', 41000, 3, 2,9),
+--   (DEFAULT, 'Hot Matcha Latte', 'minuman hangat yang populer, menggabungkan bubuk teh hijau matcha berkualitas tinggi dari Jepang dengan susu panas yang creamy. ', 41000, 21, 2,6),
+--   (DEFAULT, 'Hot Red Velvet Latte', 'minuman kopi kekinian yang memadukan espresso, susu panas, dan bubuk atau sirup red velvet, menghasilkan rasa manis, gurih, dan tekstur lembut seperti kue red velvet.', 39000, 21, 2,4),
+--   (DEFAULT, 'Ice Red Velvet Latte', 'minuman kekinian yang memadukan espresso, susu, dan sirup atau bubuk red velvet, menghasilkan rasa manis, gurih, dan tekstur creamy yang menyegarkan.', 41000, 30, 2,3),
+--   (DEFAULT, 'Hot Taro Latte', 'minuman creamy perpaduan bubuk talas ungu (taro) manis dengan susu panas, seringkali tanpa kopi (non-coffee). ', 39000, 11, 2,2),
+--   (DEFAULT, 'Ice Taro Latte', 'minuman yang menggabungkan rasa unik dari bubuk taro dan kelezatan susu.', 39000, 17, 2,1),
+--   (DEFAULT, 'Ice Lychee Tea', 'minuman teh dingin yang populer karena rasanya yang manis, segar, dan aromatik, seringkali disajikan dengan buah leci asli dan es batu.', 31000, 29, 2,1),
+--   (DEFAULT, 'Hot Regular Tea', 'No Description Yet', 19000, 17, 2,9),
+--   (DEFAULT, 'Ice Regular Tea', 'No Description Yet', 21000, 30, 2,7),
+--   (DEFAULT, 'Hot Premium Tea', 'No Description Yet', 19000, 23, 2,4),
+--   (DEFAULT, 'Ice Premium Tea', 'No Description Yet', 21000, 3, 2,1),
+--   (DEFAULT, 'On The Rock', 'cara penyajian minuman, khususnya minuman beralkohol seperti wiski atau brendi, yang disajikan di atas es batu tanpa tambahan campuran lain.', 45000, 7, 1,5),
+--   (DEFAULT, 'Piccolo Latte', 'minuman kopi susu berbasis espresso berukuran mini, biasanya disajikan dalam gelas 85-114 ml.', 30000, 39, 1,5),
+--   (DEFAULT, 'Affogatto', 'hidangan penutup (dessert) khas Italia berupa satu atau dua skup es krim vanila (atau gelato) yang disiram dengan segelas espresso panas.', 33000, 11, 1,5),
+--   (DEFAULT, 'Hot Vanilla Latte', 'minuman kopi populer yang terdiri dari campuran espresso, susu panas yang di-steam (steamed milk), dan sirup vanilla', 29000, 37, 1,5),
+--   (DEFAULT, 'Ice Vanilla Latte', 'minuman kopi populer yang terdiri dari campuran espresso, susu panas yang di-steam (steamed milk), dan sirup vanilla', 31000, 1, 1,5),
+--   (DEFAULT, 'Apple Pie', 'hidangan penutup klasik yang terdiri dari kulit pastri renyah (crust) yang diisi dengan potongan apel manis, kayu manis, dan gula. ', 32000, 14, 3,1),
+--   (DEFAULT, 'Pineapple Pie', 'hidangan penutup tropis yang populer, terkenal dengan perpaduan rasa manis dan segar dari nanas serta kulit pai yang renyah dan bermentega.', 32000, 16, 3,5),
+--   (DEFAULT, 'Almond Gallete Pie', 'kue pastri berbentuk bundar yang renyah dan berlapis-lapis (puff pastry) dengan isian krim almond manis yang lembut.', 31000, 21, 3,5),
+--   (DEFAULT, 'Cheese Danish', 'kue pastry (kue kering berlapis) yang terkenal dengan tekstur renyah di luar, lembut di dalam, serta isian krim keju manis yang gurih. ', 32000, 33, 3,5),
+--   (DEFAULT, 'Croissant Ham & Cheese', 'kombinasi sarapan atau camilan klasik yang terdiri dari roti croissant renyah yang diisi dengan ham gurih dan keju leleh, seringkali dipanggang hingga keju meleleh sempurna.', 32000, 33, 3,7),
 
--- Kebutuhan 1. DATA CARD
-SELECT product_images.path AS pictures, products.product_name AS title, products.product_desc AS description, products.price AS price
-FROM products
-JOIN product_images ON product_images_id = product_images.product_images_id
-LIMIT 4
-;
--- WHERE product_images.product_id = 1
--- AND products.id = 1;
+-- select * from discount;
+
+INSERT INTO products (product_name, product_desc, price, quantity, discount)
+VALUES
+('Kapal Api Latte', 'Fresh milk coffee', 25000, 10, 1), -- 1
+('Latte Art', 'Fresh milk coffee', 25000, 10, 1), -- 2
+('Berry Good & Ice Cream', 'idangan penutup berbahan dasar stroberi segar, sering kali berupa es krim yang dicampur dengan buah beri utuh atau saus beri kental.', 45000, 17, 2), -- 3
+('V60', 'Metode seduh kopi manual (manual brew) yang menghasilkan kopi bersih dan aromatik menggunakan dripper berbentuk V', 15000, 30, 5), -- 4
+('Cold Brew Coffee', 'metode penyeduhan kopi dengan merendam bubuk kopi giling kasar dalam air dingin/suhu ruang selama 12–24 jam, menghasilkan konsentrat kopi rendah asam, manis alami, dan halus.', 20000, 23, 7), -- 5
+('Aero press', 'No Description Yet', 21000, 3, 8), -- 6
+('Vietnam Drip', 'Metode seduh kopi khas Vietnam menggunakan saringan logam kecil, menghasilkan kopi pekat ala espresso yang biasa disajikan di atas susu kental manis.', 24000, 9, 8), --7 
+('Brownies & Ice Cream', 'No Description Yet', 28500, 5, 1), -- 8
+('Brownies Choco Chips', 'No Description Yet', 23000, 3, 8), -- 9 
+('Brownies Choco Almond', 'No Description Yet', 235000, 3, 8), -- 10
+('Cheese Danish', 'kue pastry (kue kering berlapis) yang terkenal dengan tekstur renyah di luar, lembut di dalam, serta isian krim keju manis yang gurih.', 29500, 7, 3), -- 11
+('Croissant Ham & Cheese', 'kombinasi sarapan atau camilan klasik yang terdiri dari roti croissant renyah yang diisi dengan ham gurih dan keju leleh, seringkali dipanggang hingga keju meleleh sempurna.', 30000, 3, 8), -- 12 
+('Piccolo Latte', 'minuman kopi susu berbasis espresso berukuran mini, biasanya disajikan dalam gelas 85-114 ml.', 30000, 3, 8), --13
+('Almond Gallete Pie', 'Kue pastri berbentuk bundar yang renyah dan berlapis-lapis (puff pastry) dengan isian krim almond manis yang lembut.', 13500, 1, 7), -- 14
+('Cheese Danish', 'Kue pastry (kue kering berlapis) yang terkenal dengan tekstur renyah di luar, lembut di dalam, serta isian krim keju manis yang gurih', 23000, 5, 2), --15
+('Croissant Ham & Cheese', 'Kombinasi sarapan atau camilan klasik yang terdiri dari roti croissant renyah yang diisi dengan ham gurih dan keju leleh, seringkali dipanggang hingga keju meleleh sempurna.', 24000, 11, 7) --16
+RETURNING id;
+
+INSERT INTO product_categories (product_id, category_id)
+VALUES (1,1), (2,1), (3,2), (3,5), (4,1), (5,1), (6,1), (7,1), (8,3), (8,7), (9,3), (9,7), (10,2), (10,3), (10,7), (11,2), (11,3), (11,5), (12,7), (12,2), (12,3), (13,1), (14,3), (14,7), (15,3), (15,6), (16,3), (16,7);
+
+INSERT INTO product_variant 
+  (product_variant_id, product_id, variant_name, add_price)
+  VALUES
+  (DEFAULT, 1, 'Hot', 0),
+  (DEFAULT, 2, 'Hot', 0), 
+  (DEFAULT, 3, 'Original', 0),
+  (DEFAULT, 4, 'Hot', 0), 
+  (DEFAULT, 5, 'Hot', 0), 
+  (DEFAULT, 6, 'Hot', 0), 
+  (DEFAULT, 7, 'Hot Sweet', 1000), 
+  (DEFAULT, 7, 'Hot', 0),
+  (DEFAULT, 8, 'Coklat', 0),
+  (DEFAULT, 9, 'Hot', 0), 
+  (DEFAULT, 10, 'Hot', 0),
+  (DEFAULT, 11, 'Hot', 0),
+  (DEFAULT, 12, 'Hot', 0);
+
+INSERT INTO product_size
+  (product_id, size_name, size_price)
+  VALUES (1, 'Small', 100), (1, 'Regular', 1000), (1, 'Large', 5000), (1, 'Big Boss', 10000),
+  (2, 'Regular', 0),
+  (3, 'Small', 0), (3, 'Medium', 0), (3, 'Full Cake', 5000),
+  (4, 'Small', 0), (4, 'Medium', 0), (4, 'Full Cake', 5000),
+  (5, 'Regular', 0),
+  (6, 'Small', 0), (6, 'Regular', 0), (6, 'Large', 5000), (6, 'Big Boss', 10000),
+  (7, 'Regular', 0),
+  (8, 'Small', 0),(8, 'Regular', 0),(8, 'Large', 1500),
+  (9, 'Small', 0),(9, 'Regular', 0),(9, 'Large', 1500),
+  (10, 'Small', 0),(10, 'Regular', 0),(10, 'Large', 1500),
+  (11, 'Small', 0),(11, 'Regular', 0),(11, 'Large', 1500),
+  (12, 'Small', 0),(12, 'Regular', 0),(12, 'Large', 1500);
+
+INSERT INTO
+  users
+  ("id", "full_name", "email", "password", "address", "phone", "pictures")
+  VALUES
+  (DEFAULT, 'testing admin','admin@mail.com','admin#123','maharaja, depok','0871234455','images/test/path/phone.jpg'),
+  (DEFAULT, 'normal users','users@mail.com','qwertyui','Kuningan Jakarta Selatan','0811225172','images/test/path/phone.png'),
+  (DEFAULT, 'Karto Suwiryo','kart@gmail.com','karto123','Tambun, Bekasi','0871928333','images/karto/path/kart.jpg'),
+  (DEFAULT, 'Arya Wiradatakusuma','aryawir@gmail.com','wiradata123','Jatiuwung, Tangerang','0891928333','images/arya/path/ary.jpg'),
+  (DEFAULT, 'Iskandar Dinata','iskadin@gmail.com','iskandar#111','Kiaracondong, Kota Bandung','0831927731','images/iskandar/path/dinata.jpg'),
+  (DEFAULT, 'Ahung Surawisesa','surawisesa@gmail.com','ahsur#999','Kawalu, Kota Tasikamalaya','0831998731','images/surawisesa/path/surawisesa.jpg'),
+  (DEFAULT, 'Deandles','dean@gmail.com','euy#999','Serpong, Kota Tangerang Selatan','0831991131','images/deandles/path/dean.jpg'),
+  (DEFAULT, 'Frederic Hole','frederic11@gmail.com','kfhole#1928','Gunung Putri, Bogor','0828391131','images/frederic/path/kfhole.jpg'),
+  (DEFAULT, 'Atja Sholeh','atja113@gmail.com','atja#*#111','Kawali, Ciamis','0822394131','images/atja/path/atja.jpg'),
+  (DEFAULT, 'Asep Budi','asbud@gmail.com','asep#budi*11','Ciampelas, Kota Bandung','0822394541','images/asep/path/budi.jpg');
 
 
--- Kebutuhan 2. DATA TESTIMONEY
-SELECT users.full_name AS name, users.pictures AS image ,reviews.user_id, reviews.messages, reviews.ratings
-FROM reviews
-JOIN users ON reviews.review_id = users.id
-;
+INSERT INTO
+  product_images (product_id, path)
+  VALUES
+  (1, 'test/images/kapal.heic'), 
+  (1, 'test/images/api.png'),
+  (2, 'test/images/beir.jpeg'),
+  (3, 'test/images/img.jpg'),
+  (4, 'test/images/v60.png'),
+  (5, 'test/images/v60.png'),
+  (6, 'test/images/coldbrew.heic'),
+  (7, 'test/images/aeropress.png'),
+  (8, 'test/images/brownisice.jpeg'),
+  (9, 'test/images/brownischoko.webp'),
+  (10, 'test/images/brownish.jpeg'),
+  (11, 'test/images/danish.png'),
+  (12, 'test/images/corsiant.png'),
+  (13, 'test/images/picolo.png'),
+  (14, 'test/images/almondpastry.jpg'),
+  (15, 'test/images/pastry.jpeg'),
+  (16, 'test/images/crosian.heic');
 
 
-
--- KEBUTUHAN DATA DI BROWSE PRODUCT
--- 1. CARD PRODUCT
--- data product
--- * image ~
--- * title ~
--- * is_flash_sale
--- * description ~
--- * rating 
--- * price ~
--- * price_if_discount
+INSERT INTO
+  reviews
+  (review_id, user_id, product_id, messages, ratings )
+  VALUES
+  (DEFAULT, 1, 1, 'testing reviews', 5),
+  (DEFAULT, 3, 2, 'bagus lumayan', 1),
+  (DEFAULT, 5, 3, 'sangat buruk', 4),
+  (DEFAULT, 2, 4, 'rekomended', 3),
+  (DEFAULT, 6, 5, 'cheap and good', 2),
+  (DEFAULT, 8, 6, 'hidden gem', 5),
+  (DEFAULT, 7, 7, 'art of quality', 1),
+  (DEFAULT, 4, 8, 'banyak tukang parkir', 3);
